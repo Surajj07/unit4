9b1deb0cd666b3dbcae2b07537d94eb163fc19a1
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-04-17 13:25:15 +0530

    add

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from "./problem1/Card"
+import Card2 from './problem1/card2';
 
 function App() {
   return (
     <div className="App">
-     <h1>suraj</h1>
-     <Card/>
+     
+     {/* <Card/> */}
+     <Card2/>
     </div>
   );
 }
--- day2/src/problem1/card2.jsx
+++ day2/src/problem1/card2.jsx
@@ -1,18 +1,29 @@
 import styles from "./Card.module.css";
-// import { Icon } from 'react-native-elements';
-// import { FaBeer } from 'react-icons/fa';
 
-const Card=()=>{
+
+const Card2=()=>{
  const data=[
      { date:"28/10/2020",
        study:"Case Study",
        heading:"Amazon Gift",
        subheading:"Pay",
        devices:"Desktop - Mobile",
-       logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfl1i4DTIVcDrzJUKJpl5T8xFOLDXb6JHA7d3Rkr8ZrJz-iiB2ZDEPeRxNzvnn2oCvTns&usqp=CAU",
+       logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/640px-Amazon_icon.svg.png",
        icon:"https://cdn-icons-png.flaticon.com/512/109/109617.png",
        color:"orange",
+    },
+
+     { date:"17 Sep 2020",
+       study:"Case Study",
+       heading:"Apple Gift",
+       subheading:"Payment",
+       devices:"Desktop - Mobile",
+       logo:"https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+       icon:"https://cdn-icons-png.flaticon.com/512/109/109617.png",
+       color:"grey",
+       
     }
+
  ]
 
     return (
@@ -21,34 +32,34 @@ const Card=()=>{
             data.map((elem)=>{
          return ( <div className={styles.container} style={{backgroundColor:elem.color}} >
         <div className={styles.card}>
-            {
+            {/* {
                 data.map((elem)=>{
                     return (
-                        <>
+                        <> */}
                           <h1>{elem.date}</h1>
                           <div className={styles.study}>{elem.study}</div>
                           <h1 className={styles.company}>{elem.heading}</h1>
                           <h1 className={styles.pay}>{elem.subheading}</h1>
                           <h1>{elem.devices}</h1>
-                        </>
+                        {/* </>
                     )
                 })
-            }
+            } */}
           
         </div>
         <div>
-            {
+            {/* {
                 data.map((elem)=>{
                     return (
-                        <>
+                        <> */}
                         <div className={styles.card2}>
-                        <img src={elem.logo} className={styles.logo}></img>
+                        <img src={elem.logo} className={styles.logo} ></img>
                        <img src={elem.icon} className={styles.icon}></img>
                        </div>
-                        </>
+                        {/* </>
                     )
                 })
-            }
+            } */}
         </div>
         </div>
          )
@@ -58,4 +69,4 @@ const Card=()=>{
     )
 }
 
-export default Card
+export default Card2
